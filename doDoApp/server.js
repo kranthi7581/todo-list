bd53d1d9ee503ef887d4cc544bf77becbd194316
kranthi7581 <126465569+kranthi7581@users.noreply.github.com>
2026-04-18 18:05:19 +0530

    Modify CORS settings for improved security
    Updated CORS configuration to allow specific origins.

--- doDoApp/server.js
+++ doDoApp/server.js
@@ -12,12 +12,12 @@ const PORT = process.env.PORT || 5000;
 const dns = require("dns");
 dns.setServers(["[8.8.8.8]","[1.1.1.1]"]);
 
-// app.use(cors({
-//   origin: "http://localhost:3000",
-//   "https://todolist-tau-two-70.vercel.app",
-//   credentials: true,
-// }));
-app.use(cors({ origin: "*" }));
+app.use(cors({
+  origin: "http://localhost:3000",
+  "https://todo-list-4-ww9x.onrender.com",
+  credentials: true,
+}));
+// app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("/api", authRoutes);
