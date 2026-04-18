a13816092c6aca812b30f4e96940863332d2bbfe
kranthi7581 <126465569+kranthi7581@users.noreply.github.com>
2026-04-18 18:08:39 +0530

    Refactor CORS configuration to use array for origins

--- doDoApp/server.js
+++ doDoApp/server.js
@@ -13,8 +13,10 @@ const dns = require("dns");
 dns.setServers(["[8.8.8.8]","[1.1.1.1]"]);
 
 app.use(cors({
-  origin: "http://localhost:3000",
-  "https://todo-list-4-ww9x.onrender.com",
+  origin:[
+    "http://localhost:3000",
+    "https://todo-list-4-ww9x.onrender.com",
+  ]
   credentials: true,
 }));
 // app.use(cors({ origin: "*" }));
